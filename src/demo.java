34182cb5ad8b4abd1188e940e7bb3002ede431e1
Sereinay <jy.cao@bupt.edu.cn>
2023-12-04 15:25:39 +0800

    更改

--- src/demo.java
+++ src/demo.java
@@ -2,17 +2,28 @@ import java.util.*;
 
 public class demo {
     class Solution {
-        public List<List<String>> groupAnagrams(String[] strs) {
-            HashMap<String, List<String>> AnswerMap = new HashMap<>();
-            for (String str : strs) {
-                char[] charArray = str.toCharArray();
-                Arrays.sort(charArray);
-                String key = charArray.toString();
-                List<String> list = AnswerMap.getOrDefault(key, new ArrayList<String>());
-                list.add(str);
-                AnswerMap.put(str,list);
+        public int longestConsecutive(int[] nums) {
+            HashSet<Integer> num_NoRepeat = new HashSet<>();
+            for (int num : nums) {
+                num_NoRepeat.add(num);
+//请注意这里 在使用增强for循环时 前面设置的变量即为取出来的后面的类型 请不要再按照索引方式
+// 即这种写法  num_NoRepeat.add(nums[num]); 是错误的！！！
             }
-            return new ArrayList<List<String>>(AnswerMap.values());
+//把数组的数据结构转换成HashMap 去掉其中的重复元素
+            int longestBreak = 0;
+            for (int i : num_NoRepeat) {
+                if (!num_NoRepeat.contains(i-1)){
+                    int CurrentNum = i;
+                    int CurrentBreak = 1;
+
+                    while (num_NoRepeat.contains(CurrentNum+1)){
+                        CurrentBreak++;
+                        CurrentNum++;
+                    }
+                    longestBreak = Math.max(longestBreak, CurrentBreak);
+                }
+            }
+            return longestBreak;
         }
     }
 }
