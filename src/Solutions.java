fa167d8971e780304352fb5be95ffff27516c575
Sereinay <jy.cao@bupt.edu.cn>
2024-01-18 00:39:09 +0800

    假期刚开始写的两道题

--- src/Solutions.java
+++ src/Solutions.java
@@ -1,3 +1,9 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Solutions {
     //    239.滑动窗口最大值
     public int[] maxSlidingWindow(int[] nums, int k) {
@@ -58,5 +64,41 @@ public class Solutions {
 //        但是这样写的话可读性不好hhh
         return ans;
     }
-}
+
 //      进行了空间复杂度优化的代码
+
+    //    14.合并区间
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0]-o2[0];
+            }
+        });//重写了比较器 把二维数组按照第一个元素来升序排列
+        List<int[]> merged = new ArrayList<int[]>();
+        for (int[] interval : intervals) {
+            int L = interval[0];
+            int R = interval[1];
+//            用L R来存一个节点的左右端点值
+            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < L) {
+                merged.add(new int[]{L, R});
+            } else
+                merged.get(merged.size() - 1)[1] = Math.max(R, merged.get(merged.size() - 1)[1]);
+        }
+//     先把interval的第一组L R拿出来 merged非空（初始化用）或merged末尾的右侧元素小于当前的L（即不会出现合并时 直接把L R添加进即可）
+//     否则的话 维护更新merged的末尾的右端元素 使其为合并后的区间最长值即可
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+//          15.轮转数组
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] temp=new int[n];
+        for (int i = 0; i < n; i++) {
+            temp[(i+k)%n]=nums[i];
+        }
+        System.arraycopy(temp,0,nums,0,n);
+    }
+//    很简单一道题目 类似循环列表 算出来移动后的坐标后 开一个新数组来存 最后memcopy就行
+//    要注意的点就是System.arraycopy五个参数 还有%运算优先级高于+ 写的时候要记得带括号
+}
