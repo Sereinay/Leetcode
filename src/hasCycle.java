fe42f448b61bd534f9c7961b194da6def7ade3d9
Sereinay <jy.cao@bupt.edu.cn>
2024-01-23 23:27:22 +0800

    today

---
+++ src/hasCycle.java
@@ -0,0 +1,26 @@
+public class hasCycle {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (fast != slow) {
+            if (fast == null || fast.next == null)
+                return false;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+}
--- src/isPalindrome.java
+++ src/isPalindrome.java
@@ -29,5 +29,51 @@ public class isPalindrome {
             }
             return true;
         }
+
+        class Solution_1 {
+            public boolean isPalindrome(ListNode head) {
+                if (head==null)
+                    return true;
+
+                ListNode firstEnd=EndOfFirst(head);
+                ListNode secondStart=reverseListNode(firstEnd.next);
+
+                ListNode p1=head;
+                ListNode p2=secondStart;
+                boolean result=true;
+                while (result&p2!=null){
+                    if (p1.val!= p2.val)
+                        result= false;
+                    p1=p1.next;
+                    p2=p2.next;
+                }
+                firstEnd.next=reverseListNode(secondStart);
+                return result;
+            }
+
+            private ListNode reverseListNode(ListNode secondStart) {
+//                这个函数对链表进行反转
+                ListNode pre=null;
+                ListNode curr=secondStart;
+                while (curr!=null){
+                    ListNode next=secondStart.next;
+                    curr.next=pre;
+                    pre=curr;
+                    curr=next;
+                }
+                return pre;
+            }
+
+            private ListNode EndOfFirst(ListNode head) {
+//                这个函数找前半部分链表末尾
+                ListNode fast=head;
+                ListNode slow=head;
+                while (fast.next!=null&&fast.next.next!=null){
+                    fast=fast.next.next;
+                    slow=slow.next;
+                }
+                return slow;
+            }
+        }
     }
 }
